cc98a66832e65026aa0925754c02d51c457b85ac
jameshancock144e <james.hancock144e@gmail.com>
2017-06-18 02:09:05 +0100

    New update notification dialog

--- src/auc.py
+++ src/auc.py
@@ -3,12 +3,11 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from aucgtk import NotificationDialog
+from aucgtk import UpdateNotificationDialog
 from aucpacman import getUpdateCount, syncDB
 
 syncDB() # Update pacman database
-if (getUpdateCount() > 0): # Check for updates
-    notify = NotificationDialog("<big>" + str(getUpdateCount()) + " updates are available</big>") # Alert user to updates
-    notify.connect("delete-event", Gtk.main_quit)
-    notify.show_all()
-    Gtk.main()
+notify = UpdateNotificationDialog("<big>" + str(getUpdateCount()) + " updates are available</big>") # Alert user to updates
+notify.connect("delete-event", Gtk.main_quit)
+notify.show_all()
+Gtk.main()
--- src/aucgtk.py
+++ src/aucgtk.py
@@ -4,11 +4,19 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class NotificationDialog(Gtk.Window): # Dialog for displaying message to user
+class UpdateNotificationDialog(Gtk.Window): # Dialog for displaying message to user
     def __init__(self, text):
         Gtk.Window.__init__(self, title="AUC") # Create window
-        box = Gtk.Box(spacing=6) # Create box
-        self.add(box)
+        grid = Gtk.Grid() # Create box
+        self.add(grid)
         label = Gtk.Label() # Create and set label to input
-        box.add(label)
         label.set_markup(text)
+        grid.attach(label, 0, 0, 2, 1)
+        updateButton = Gtk.Button("Update")
+        viewButton = Gtk.Button("View")
+        grid.attach(viewButton, 0, 1, 1, 1)
+        grid.attach(updateButton, 1, 1, 1, 1)
+
+class UpdateStatusWindow(Gtk.Window):
+    def __init(self):
+        print()
