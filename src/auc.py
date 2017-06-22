f599e1a7c74114620e720b5320590f0e1f579c34
jameshancock144e <james.hancock144e@gmail.com>
2017-06-22 19:28:10 +0100

    Some commenting and changes for uniformity

--- src/auc.py
+++ src/auc.py
@@ -3,12 +3,12 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from aucgtk import UpdateNotificationDialog
+from aucgtk import UpdateNotificationWindow
 from aucpacman import getUpdateCount, syncDB
 
 syncDB() # Update pacman database
 if (getUpdateCount() > 0):
-    notify = UpdateNotificationDialog("<big>" + str(getUpdateCount()) + " updates are available</big>") # Alert user to updates
+    notify = UpdateNotificationWindow("<big>" + str(getUpdateCount()) + " updates are available</big>") # Alert user to updates
     notify.connect("delete-event", Gtk.main_quit)
     notify.show_all()
     Gtk.main()
