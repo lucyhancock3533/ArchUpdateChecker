f70a3cb31dc95384ad6bcf71745c9200570abaed
jameshancock144e <jameshancock144e@users.noreply.github.com>
2017-06-22 15:18:30 +0100

    Pull 0.2-dev down into master (#2)
    * Update README.md
    
    * New update notification dialog
    
    * Update process
    
    * View updates window, and code to get updates for window
    
    * View button
    
    * Update GUI
    
    * Changed update command
    
    * Update window
    
    * Update window scrolling
    
    * Added 0 update check

--- src/auc.py
+++ src/auc.py
@@ -3,12 +3,12 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from aucgtk import NotificationDialog
+from aucgtk import UpdateNotificationDialog
 from aucpacman import getUpdateCount, syncDB
 
 syncDB() # Update pacman database
-if (getUpdateCount() > 0): # Check for updates
-    notify = NotificationDialog("<big>" + str(getUpdateCount()) + " updates are available</big>") # Alert user to updates
+if (getUpdateCount() > 0):
+    notify = UpdateNotificationDialog("<big>" + str(getUpdateCount()) + " updates are available</big>") # Alert user to updates
     notify.connect("delete-event", Gtk.main_quit)
     notify.show_all()
     Gtk.main()
