58b7c0ccf657449bd7abcc8766f16bd4ea40b060
Lucy Hancock <lucy@leh.dev>
2023-06-04 17:58:41 +0100

    Added startup code for CLI

--- src/auc.py
+++ src/auc.py
@@ -1,10 +1,17 @@
 #! /usr/bin/python3
 """AUC Main module, syncs, checks for updates, and alerts user."""
 
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-from aucgtk import run_auc_gtk
+from sys import argv
+from auccli import run_auc_cli
 
 if(__name__ == "__main__"):
-    run_auc_gtk()
+    if(len(argv) > 1):
+        run_auc_cli()
+    else:
+        try:
+            import gi
+            gi.require_version('Gtk', '3.0')
+            from aucgtk import run_auc_gtk
+            run_auc_gtk()
+        except ImportError:
+            print("[AUC] GTK not available and no parameters passed")
