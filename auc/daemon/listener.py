03886ead6ede5b997830a5fa541b328e0f727f4b
Lucy Hancock <lucy@leh.dev>
2023-06-04 17:38:31 +0100

    Add listen loop

--- auc/daemon/listener.py
+++ auc/daemon/listener.py
@@ -1,10 +1,13 @@
 import secrets
 
+from pathlib import Path
 from multiprocessing.connection import Listener
 
 
 class DaemonListener:
-    def __init__(self):
+    def __init__(self, args, logger):
+        self.args = args
+        self.logger = logger
         self._init_secret()
         self.listener = Listener('/tmp/.auc_socket', family='AF_UNIX', authkey=self.secret.encode())
 
@@ -14,7 +17,20 @@ class DaemonListener:
             f.write(self.secret)
 
     def listen_loop(self):
-        conn = self.listener.accept()
-        while True:
-            msg = conn.recv()
-            print(msg)
+        try:
+            self.logger.info('Starting daemon listener')
+            while True:
+                conn = self.listener.accept()
+                while True:
+                    try:
+                        msg = conn.recv()
+                    except EOFError:
+                        break
+                    print(msg)
+                conn.close()
+        except KeyboardInterrupt:
+            self.listener.close()
+            Path('/tmp/.auc_socket').unlink()
+            Path('/tmp/.auc_secret').unlink()
+            self.logger.info('Exiting')
+
