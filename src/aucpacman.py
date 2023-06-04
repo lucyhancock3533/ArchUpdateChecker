eea25fa853574ca4ffa7e4a670c51232df857704
Lucy Hancock <lucy@leh.dev>
2023-06-04 17:56:48 +0100

    Moved to using gksudo while running as opposed to running as root

--- src/aucpacman.py
+++ src/aucpacman.py
@@ -4,7 +4,7 @@
 import subprocess, os, sys
 
 def syncDB(): # Invoke pacman and update package database
-    syncProcess = subprocess.Popen(["/usr/bin/pacman", "-Syy"])
+    syncProcess = subprocess.Popen(["/usr/bin/gksudo", "/usr/bin/pacman -Syy"])
     syncProcess.wait()
 
 def getUpdateCount(): # Get number of packages needing updates
@@ -12,7 +12,7 @@ def getUpdateCount(): # Get number of packages needing updates
     return int(updates.read())
 
 def runUpdates(): # Run pacman and update system
-    updates = subprocess.Popen(["/usr/bin/pacman", "-Su", "--noconfirm", "--noprogressbar"], stdout=subprocess.PIPE)
+    updates = subprocess.Popen(["/usr/bin/gksudo", "/usr/bin/pacman -Su --noconfirm --noprogressbar"], stdout=subprocess.PIPE)
     return updates
 
 def getUpdates(): # Get list of updates
