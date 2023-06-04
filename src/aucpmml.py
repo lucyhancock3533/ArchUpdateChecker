e91f5192420a3951cf93084f9148cf22956a1d9c
Lucy Hancock <lucy@leh.dev>
2023-06-04 17:57:49 +0100

    Updating of mirrorlist

--- src/aucpmml.py
+++ src/aucpmml.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 """Module for dealing with mirrorlist syncing"""
 
-import os
+import os, requests
 from pathlib import Path
 
 def get_mrl_url():
@@ -28,3 +28,16 @@ def set_mrl_url(new_url):
     file.write(new_url)
     file.write('\n')
     file.close() # Write settings to file
+
+def update_mrl():
+    url = get_mrl_url()
+    mrl_req =  requests.get(url)
+    config_folder = Path(os.path.expanduser("~/.auc/"))
+    config_folder.mkdir(parents=True, exist_ok=True) # Ensure folder exists
+    if mrl_req.status_code != requests.codes.ok:
+        print("Failed to update mirrorlist")
+        return
+    file = open(os.path.expanduser("~/.auc/mirrorlist.tmp"), 'w')
+    file.write(mrl_req.text.replace('#S', 'S')) # Remove commenting from generated mirrorlist
+    file.close()
+    os.popen("gksudo mv ~/.auc/mirrorlist.tmp /etc/pacman.d/mirrorlist")
