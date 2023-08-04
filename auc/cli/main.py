581ebb42bf59106e2d76f261465b2d7ddb23cd45
Lucy Hancock <lucy@leh.dev>
2023-08-05 00:37:58 +0100

    Start 1.4 dev

--- auc/cli/main.py
+++ auc/cli/main.py
@@ -11,7 +11,7 @@ log_levels = {'error': logging.ERROR, 'warning': logging.WARNING, 'info': loggin
 
 
 def version(logger):
-    logger.info('AUC v1.3.1')
+    logger.info('AUC v1.4.0')
 
 
 def status_cmd(logger):
