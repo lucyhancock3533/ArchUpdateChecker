7d79512b6cd543363c42c4b0a28eb44ac72ce608
Lucy Hancock <lucy@leh.dev>
2023-12-12 23:38:01 +0800

    Further testing

--- auc/daemon/listener.py
+++ auc/daemon/listener.py
@@ -9,10 +9,12 @@ class DaemonListener:
         self.args = args
         self.logger = logger
         self._init_secret()
+        Path('/tmp/.auc_socket').unlink(missing_ok=True)
         self.listener = Listener('/tmp/.auc_socket', family='AF_UNIX', authkey=self.secret.encode())
 
     def _init_secret(self):
         self.secret = secrets.token_hex(nbytes=512)
+        Path('/tmp/.auc_secret').unlink(missing_ok=True)
         with open('/tmp/.auc_secret', 'w') as f:
             f.write(self.secret)
 
@@ -24,17 +26,14 @@ class DaemonListener:
                 while True:
                     try:
                         msg = conn.recv()
+                        print(msg)
+                        conn.send('recv')
                     except EOFError:
+                        conn.close()
                         break
-                    print(msg)
-                    conn.send('recv')
-                conn.close()
         except KeyboardInterrupt:
             self.listener.close()
-            try:
-                Path('/tmp/.auc_socket').unlink()
-            except FileNotFoundError:
-                pass
-            Path('/tmp/.auc_secret').unlink()
+            Path('/tmp/.auc_socket').unlink(missing_ok=True)
+            Path('/tmp/.auc_secret').unlink(missing_ok=True)
             self.logger.info('Exiting')
 
