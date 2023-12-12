7338d77b96ba437c78a34c057589e50281318a83
Lucy Hancock <lucy@leh.dev>
2023-12-12 23:38:17 +0800

    Add basic prompter window functionality

--- auc/client/daemon_conn.py
+++ auc/client/daemon_conn.py
@@ -7,3 +7,7 @@ def get_status():
 
 def get_updates():
     return requests.post('http+unix://%2Ftmp%2F.auc_socket/', json={'function': 'updates'})
+
+
+def get_prompt():
+    return requests.post('http+unix://%2Ftmp%2F.auc_socket/', json={'function': 'prompt'})
