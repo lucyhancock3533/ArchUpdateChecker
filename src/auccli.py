8a1eb0a83ad7c311e30e277ff5e541cabac823e2
Lucy Hancock <lucy@leh.dev>
2023-12-12 23:37:57 +0800

    Added logging to CLI mode

--- src/auccli.py
+++ src/auccli.py
@@ -16,20 +16,29 @@ def run_auc_cli():
         run_auc()
 
 def run_auc():
+	log = Logger()
     sync_db(["/usr/bin/sudo", "pacman", "-Sy"])
-    print(str(get_update_count()) + " updates are available, " + str(get_ignore_count())\
-    + " updates are ignored")
+	log.log_write("AUC running in CLI mode")
+	note_str = str(get_update_count()) + " updates are available, " + str(get_ignore_count())\
+    + " updates are ignored"
+    print(note_str)
+	log.log_write(note_str)
     if (get_update_count() > 0):
-        prompt_updates()
+        prompt_updates(log)
+	log.log_close()
 
-def prompt_updates():
+def prompt_updates(log):
     updates = [upd.decode("utf-8") for upd in get_updates()]
-    print("Updates available: " + ', '.join(updates))
+	upd_str = "Updates available: " + ', '.join(updates)
+    print(upd_str)
+	log.log_write(upd_str)
     check_upd = input("Update now?(y/n) ")
     if check_upd == 'y':
-        do_updates()
+        do_updates(log)
 
-def do_updates():
+def do_updates(log):
     update_pipe = run_updates(["/usr/bin/sudo", "pacman", "-Su", "--noconfirm", "--noprogressbar"])
     for line in update_pipe.stdout: # Update text view
         print(line.decode("utf-8"))
+		log.log_write(line.decode("utf-8"))
+	log.log_write("AUC updates are complete")
