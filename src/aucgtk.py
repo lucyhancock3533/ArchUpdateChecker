82dbe425ed94e00143924f50f13b38da025a3290
Lucy Hancock <lucy@leh.dev>
2023-12-12 23:36:09 +0800

    Pull 0.2-dev down into master (#2)
    * Update README.md
    
    * New update notification dialog
    
    * Update process
    
    * View updates window, and code to get updates for window
    
    * View button
    
    * Update GUI
    
    * Changed update command
    
    * Update window
    
    * Update window scrolling
    
    * Added 0 update check

--- src/aucgtk.py
+++ src/aucgtk.py
@@ -1,14 +1,65 @@
 #! /usr/bin/python3
 
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+import threading
+from gi.repository import GLib, Gtk, GObject
+from aucpacman import getUpdates, runUpdates
 
-class NotificationDialog(Gtk.Window): # Dialog for displaying message to user
+class UpdateNotificationDialog(Gtk.Window): # Dialog for displaying message to user
     def __init__(self, text):
         Gtk.Window.__init__(self, title="AUC") # Create window
-        box = Gtk.Box(spacing=6) # Create box
-        self.add(box)
+        grid = Gtk.Grid() # Create component grid
+        self.add(grid)
         label = Gtk.Label() # Create and set label to input
-        box.add(label)
         label.set_markup(text)
+        grid.attach(label, 0, 0, 2, 1)
+        updateButton = Gtk.Button("Update") # Add update button
+        viewButton = Gtk.Button("View") # Add view updates button
+        viewButton.connect("clicked", self.launchViewWindow)
+        updateButton.connect("clicked", self.launchUpdateWindow)
+        grid.attach(viewButton, 0, 1, 1, 1)
+        grid.attach(updateButton, 1, 1, 1, 1)
+
+    def launchViewWindow(self, button):
+        window = UpdateViewWindow(getUpdates())
+        window.show_all()
+
+    def launchUpdateWindow(self, button):
+        window = UpdateStatusWindow()
+        window.show_all()
+
+class UpdateViewWindow(Gtk.Window):
+    def __init__(self, updates):
+        Gtk.Window.__init__(self, title="AUC") # Create window
+        listbox = Gtk.ListBox() # Create list of updates
+        for update in updates: # Populate update list
+            label = Gtk.Label(update.decode("utf-8"))
+            row = Gtk.ListBoxRow()
+            row.add(label)
+            listbox.add(row)
+        self.add(listbox)
+
+class UpdateStatusWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="AUC") # Create window
+        self.set_default_size(800, 600) # Set window size
+        scrolledwindow = Gtk.ScrolledWindow() # Create scrolled container
+        scrolledwindow.set_hexpand(True)
+        scrolledwindow.set_vexpand(True)
+        self.add(scrolledwindow)
+        self.textview = Gtk.TextView() # Create text view
+        self.textbuffer = self.textview.get_buffer()
+        self.textview.set_editable(False)
+        self.textview.set_cursor_visible(False)
+        self.textview.set_wrap_mode(2)
+        scrolledwindow.add(self.textview)
+        self.updatesthread = threading.Thread(target=self.doUpdates) # Start updates thread
+        self.updatesthread.start()
+
+    def updateProgress(self, progress):
+        self.textbuffer.insert_at_cursor(progress) # Add progress to text view
+        self.textview.scroll_to_mark(self.textbuffer.get_insert(), 0.0, True, 0.5, 0.5)
+
+    def doUpdates(self):
+        updates = runUpdates() # Run updates
+        for line in updates.stdout: # Update text view
+            GLib.idle_add(self.updateProgress, line.decode())
