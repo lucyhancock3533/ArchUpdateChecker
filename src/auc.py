ea32c7d4e25d99da879c59b65cd61f8b41be90f3
Lucy Hancock <lucy@leh.dev>
2023-12-12 23:37:53 +0800

    Catch Ctrl-C and actually exit

--- src/auc.py
+++ src/auc.py
@@ -5,13 +5,16 @@ from sys import argv
 from auccli import run_auc_cli
 
 if(__name__ == "__main__"):
-    if(len(argv) > 1):
-        run_auc_cli()
-    else:
-        try:
-            import gi
-            gi.require_version('Gtk', '3.0')
-            from aucgtk import run_auc_gtk
-            run_auc_gtk()
-        except ImportError:
-            print("[AUC] GTK not available and no parameters passed")
+    try:
+        if(len(argv) > 1):
+            run_auc_cli()
+        else:
+            try:
+                import gi
+                gi.require_version('Gtk', '3.0')
+                from aucgtk import run_auc_gtk
+                run_auc_gtk()
+            except ImportError:
+                print("[AUC] GTK not available and no parameters passed")
+    except KeyboardInterrupt:
+        quit()
