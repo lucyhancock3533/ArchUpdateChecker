7de3ab57d6538c45bc33714df6f87f6f5c86ec21
Lucy Hancock <lucy@leh.dev>
2023-12-12 23:38:02 +0800

    Add status listener

---
+++ auc/daemon/state.py
@@ -0,0 +1,25 @@
+from threading import Lock
+
+
+class AucState:
+    def __init__(self):
+        self.lock = Lock()
+        self.state = {
+            'msg': 'Updates in progress',
+            'prompt': False,
+            'mirrorlist': True,
+            'update': True,
+            'inprogress': True,
+            'rebootrequired': False
+        }
+
+    def access_state(self, key):
+        self.lock.acquire()
+        v = self.state[key]
+        self.lock.release()
+        return v
+
+    def set_state(self, key, val):
+        self.lock.acquire()
+        self.state[key] = val
+        self.lock.release()
