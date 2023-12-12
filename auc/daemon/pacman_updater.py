3fdf1a0d4e09933a71eb1c9df1858bf40937bce7
Lucy Hancock <lucy@leh.dev>
2023-12-12 23:38:10 +0800

    Improve pacman log file

--- auc/daemon/pacman_updater.py
+++ auc/daemon/pacman_updater.py
@@ -12,11 +12,16 @@ class PacmanUpdater:
         try:
             paccmd = subprocess.run(['pacman', '-Sy'], check=True, capture_output=True, text=True)
             with open(f'{self.log_path}/auc_pacman_{datetime.today().strftime("%Y-%m-%d")}.log', 'a') as f:
+                f.write('##### pacman -Sy stdout #####')
                 f.write(paccmd.stdout)
+                f.write('##### pacman -Sy stderr #####')
                 f.write(paccmd.stderr)
+                f.write('\n')
         except subprocess.CalledProcessError as e:
             with open(f'{self.log_path}/auc_pacman_{datetime.today().strftime("%Y-%m-%d")}.log', 'a') as f:
+                f.write('##### pacman -Sy stderr #####')
                 f.write(e.stderr)
+                f.write('\n')
             raise e
 
     def get_updates(self):
@@ -26,12 +31,15 @@ class PacmanUpdater:
             updates_list = paccmd.stdout.split('\n')
             split_updates = [x.split(' ') for x in updates_list if len(x) > 1]
             with open(f'{self.log_path}/auc_pacman_{datetime.today().strftime("%Y-%m-%d")}.log', 'a') as f:
-                f.write('\nUpdates available:\n')
+                f.write('Updates available:\n')
                 f.write(paccmd.stdout)
+                f.write('\n')
             return {x[0]: {'old': x[1], 'new': x[3]} for x in split_updates}
         except subprocess.CalledProcessError  as e:
             with open(f'{self.log_path}/auc_pacman_{datetime.today().strftime("%Y-%m-%d")}.log', 'a') as f:
+                f.write('##### pacman -Qnu stderr #####')
                 f.write(e.stderr)
+                f.write('\n')
             raise e
 
     def do_updates(self):
@@ -40,9 +48,14 @@ class PacmanUpdater:
             paccmd = subprocess.run(['pacman', '-Su', '--noconfirm', '--noprogressbar'],
                                     check=True, capture_output=True, text=True)
             with open(f'{self.log_path}/auc_pacman_{datetime.today().strftime("%Y-%m-%d")}.log', 'a') as f:
+                f.write('##### pacman -Su --noconfirm --noprogressbar stdout #####')
                 f.write(paccmd.stdout)
+                f.write('##### pacman -Su --noconfirm --noprogressbar stderr #####')
                 f.write(paccmd.stderr)
+                f.write('\n')
         except subprocess.CalledProcessError as e:
             with open(f'{self.log_path}/auc_pacman_{datetime.today().strftime("%Y-%m-%d")}.log', 'a') as f:
+                f.write('##### pacman -Su --noconfirm --noprogressbar stderr #####')
                 f.write(e.stderr)
+                f.write('\n')
             raise e
